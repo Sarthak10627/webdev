c74ebf350f25531254a0858e757c84b48fb52ce8
Sarthak <skkaundal9314@gmail.com>
2024-02-11 14:26:01 +0530

    day 16 completed

---
+++ day16/main.js
@@ -0,0 +1,22 @@
+//npm - node package manager
+//v8  JS engine used by chrome
+
+
+// - npm
+// semantic versioning
+// 1.70.0
+
+// 1st digit: 
+// major version
+//     - new CSSFontFeatureValuesRule
+//     - not backward complete - this will break existing functionality
+
+// 2nd digits:
+// minor version
+// - small updates to the package
+// - generally backward compatible
+
+// last digit:
+// patch
+// - small  bug fixes
+// - backward compatible
