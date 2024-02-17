92d25d348a7f72e8ce8c22b47809efe60d0688f8
Sarthak <149752346+Sarthak10627@users.noreply.github.com>
2024-02-17 14:01:19 +0530

    Update common.js

--- day19/common.js
+++ day19/common.js
@@ -4,14 +4,23 @@ export const EXPIRES_IN = "EXPIRES_IN";
 
 const APP_URL = import.meta.env.VITE_APP_URL;
 
- export const ENDPOINT = {
-    userInfo : "me",
-    featuredPlaylist : "browse/featured-playlists?limit=5"
+export const ENDPOINT = {
+    userInfo: "me",
+    featuredPlaylist: "browse/featured-playlists?limit=5",
+    toplists:
+        "browse/categories/toplists/playlists?limit=10",
+    playlist: "playlists"
+    
 }
 
 export const logout = () => {
     localStorage.removeItem(ACCESS_TOKEN);
     localStorage.removeItem(EXPIRES_IN);
-    localStorage.removeItem(TOKEN_TYPE);        
+    localStorage.removeItem(TOKEN_TYPE);
     window.location.href = APP_URL
+}
+
+export const SECTIONTYPE = {
+    DASHBOARD: "DASHBOARD",
+    PLAYLIST: "PLAYLIST"
 }
